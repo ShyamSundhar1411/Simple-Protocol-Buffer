e8f5e82122972524c92c3a094e6f60a40118edf9
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-12-28 10:11:12 +0530

    Binary Data

---
+++ index_proto.js
@@ -0,0 +1,18 @@
+const Schema = require("./employees_pb");
+const fs = require("fs")
+const test1 = new Schema.Employee();
+test1.setId(1011);
+test1.setName("Test1");
+test1.setSalary(10000);
+
+
+const test2 = new Schema.Employee();
+test2.setId(1013);
+test2.setName("Test2");
+test2.setSalary(10001);
+
+const employees = new Schema.Employees();
+employees.addEmployees(test1);
+employees.addEmployees(test2);
+const bytes = employees.serializeBinary()
+fs.writeFileSync("employeeBinary",bytes)
